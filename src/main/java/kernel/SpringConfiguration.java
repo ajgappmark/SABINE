0a5dc8e2beba69b64f62bed2bb6fa700c504e52f
pchopinet <pierre.chopinet97@gmail.com>
2018-06-20 11:34:42 +0200

    create submodule

--- src/main/java/kernel/SpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package kernel;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
-
-@Component
-@ComponentScan(basePackages = {"bean", "bot", "dao", "kernel", "runnable", "supervisor"})
-public class SpringConfiguration {
-}
