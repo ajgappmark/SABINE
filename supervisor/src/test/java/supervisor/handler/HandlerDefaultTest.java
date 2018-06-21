b1a939553eec506d73bb1c36a72916e0ca379a82
pchopinet <pierre.chopinet97@gmail.com>
2018-06-21 15:35:56 +0200

    test for handler default

--- supervisor/src/test/java/supervisor/handler/HandlerDefaultTest.java
+++ supervisor/src/test/java/supervisor/handler/HandlerDefaultTest.java
@@ -9,19 +9,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HandlerDefaultTest extends TestCase {
-    int port = 9950;
-
-    @BeforeEach
-    protected void setUp() {
-
-    }
-
-    @AfterEach
-    protected void tearDown() {
-    }
+    private HandlerDefault d = new HandlerDefault();
+    private int port = 9950;
 
     @Test
     void getResponse() {
-
+        assertEquals("<h1>Server start success if you see this message</h1>" + "<h1>Port: " + port + "</h1>", d.getResponse(port));
     }
 }
