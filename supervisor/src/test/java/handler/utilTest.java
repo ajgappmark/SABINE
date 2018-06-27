8aaeb8e8f355dc4eb11a188fac4e96ee0f7d43c1
pchopinet <pierre.chopinet97@gmail.com>
2018-06-27 11:30:07 +0200

    add test for handler.util

--- supervisor/src/test/java/handler/utilTest.java
+++ supervisor/src/test/java/handler/utilTest.java
@@ -3,9 +3,12 @@ package handler;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
 
 public class utilTest {
+    private String a = "id=jeej&ip=localhost";
 
     @Before
     public void setUp() throws Exception {
@@ -14,6 +17,7 @@ public class utilTest {
 
     @Test
     public void getPOSTFromString() {
-
+        HashMap<String, String> post = util.getPOSTFromString(a);
+        assertEquals("{ip=localhost, id=jeej}", post.toString());
     }
 }
